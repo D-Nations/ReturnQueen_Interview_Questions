9571b62cfc16628bc255bf674cee4b88d13840dd
Daniel Nations <dnations@gmail.com>
2022-01-26 13:49:11 +0900

    More complete testing, can't resolve solution output order.

--- fuzzy_phrases/solution.py
+++ fuzzy_phrases/solution.py
@@ -12,7 +12,7 @@ def phrasel_search(P, Queries):
                 if word == phrase_list[0]:
                     p_index = 0
                     q_index = i
-                    while p_index < len(phrase_list) and q_index < len(query_list):
+                    while True:
                         if phrase_list[p_index] == query_list[q_index] and p_index == len(phrase_list) - 1 and q_index == len(query_list) - 1:
                             matches.append(' '.join(query_list[i:]))
                             break
@@ -31,12 +31,27 @@ def phrasel_search(P, Queries):
     return ans
 
 if __name__ == "__main__":
-    with open('50_points.json', 'r') as f:
+    with open('sample.json', 'r') as f:
         sample_data = json.loads(f.read())
         P, Queries = sample_data['phrases'], sample_data['queries']
         returned_ans = phrasel_search(P, Queries)
-        for i in range(len(Queries)):
-            if sorted(returned_ans[i]) == sorted(sample_data['solution'][i]):
-                print('============= TEST PASSED SUCCESSFULLY ===============')
-            else:
-                print(f'\nreturned answer: {sorted(returned_ans[i])}\nexpected answer: {sorted(sample_data["solution"][i])}')
+        print('============= ALL TEST PASSED SUCCESSFULLY ===============')
+
+'''
+tests = ['sample.json', '20_points.json', '30_points.json', '50_points.json']
+
+if __name__ == "__main__":
+    all_pass = True
+    for test in tests:
+        with open(test, 'r') as f:
+            print(test)
+            sample_data = json.loads(f.read())
+            P, Queries = sample_data['phrases'], sample_data['queries']
+            returned_ans = phrasel_search(P, Queries)
+            for i in range(len(Queries)):
+                if sorted(returned_ans[i]) != sorted(sample_data['solution'][i]):
+                    all_pass = False
+                    print(f'query #{i}\nreturned answer: {sorted(returned_ans[i])}\nexpected answer: {sorted(sample_data["solution"][i])}')
+    if all_pass:
+        print('All tests passed!')
+'''
